4a5776a796558d714fd7bb0887feef6f12993bd2
Voahary Estelle Rajerison <voahary-estelle.rajerison@polymtl.ca>
2021-01-26 14:00:52 -0500

    exercice 4 essayé

--- exercice.py
+++ exercice.py
@@ -17,7 +17,8 @@ def average(a: float, b: float, c: float) -> float:
 
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
-    return 0.0
+    decimaux = angle_degs + (angle_mins + angle_secs/60)/60
+    return decimaux * math.pi/180
 
 
 def to_degrees(angle_rads: float) -> tuple:

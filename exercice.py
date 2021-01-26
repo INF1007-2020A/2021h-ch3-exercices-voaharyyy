4eb12c39b6e58a16ff65ac0d8ff0d5b877384374
Voahary Estelle Rajerison <voahary-estelle.rajerison@polymtl.ca>
2021-01-26 14:15:24 -0500

    exercice 4 essayé

--- exercice.py
+++ exercice.py
@@ -17,8 +17,8 @@ def average(a: float, b: float, c: float) -> float:
 
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
-    decimaux = angle_degs + (angle_mins + angle_secs/60)/60
-    return decimaux * math.pi/180
+    decimaux = angle_degs + (angle_mins/60 + angle_secs/3600)
+    return math.radians(decimaux)
 
 
 def to_degrees(angle_rads: float) -> tuple:
@@ -40,7 +40,7 @@ def main() -> None:
 
     print(f"Moyenne des nombres 2, 4, 6: {average(2, 4, 6)}")
 
-    print(f"Conversion de 100 degres, 2 minutes et 45 secondes en radians: {to_radians(180, 2, 45)}")
+    print(f"Conversion de 100 degres, 2 minutes et 45 secondes en radians: {to_radians(100, 2, 45)}")
     
     degrees, minutes, seconds = to_degrees(1.0)
     print(f"Conversion de 1 radian en degres: {degrees} degres, {minutes} minutes et {seconds} secondes")
